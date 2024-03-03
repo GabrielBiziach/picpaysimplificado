cd658c423c2a9878b508b0ee9a5ceaf4281cfbe5
gabrielbiziach <gabrielbiziach@gmail.com>
2024-03-03 19:19:52 -0300

    feat: create UserController and validations

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -2,11 +2,13 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.users.User;
 import com.picpaysimplificado.domain.users.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -29,6 +31,16 @@ public class UserService {
 
     public void saveUser(User user) {
         this.repository.save(user);
+    }
+
+    public User createUser(UserDTO userDTO) {
+        User newUser = new User(userDTO);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
     }
 
 }
